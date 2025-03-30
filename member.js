e01011b0b9dfa92e80b4dda7b0d060ffe0b75c2c
maximilianoPaciEducativo <mpaci@palermo.edu>
2025-03-30 21:53:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h3>Skills</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  );
+}
